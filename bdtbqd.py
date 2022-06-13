506453fd3748d9f744c518e8d825242f2973b648
KS <102157799+l5655655@users.noreply.github.com>
2022-06-13 19:39:51 +0800

    Update bdtbqd.py

--- bdtbqd.py
+++ bdtbqd.py
@@ -1,14 +1,19 @@
 import requests
 import os
-
+import time
+import json
 
 bdtbck = os.getenv('bdtbcookie')
 bdtbqd = os.getenv('bdtbqdmc')
 bdtbqds=list(str("%s" % bdtbqd).split("&"))
-print(bdtbqds)
+print("===================百度贴吧签到脚本=====================")
+time_tuple = time.localtime(time.time())
+print("北京时间:{}年{}月{}日{}点{}分{}秒".format(time_tuple[0],time_tuple[1],time_tuple[2],time_tuple[3],time_tuple[4],time_tuple[5]))
+print("======================================================")
 for i in range(len(bdtbqds)):
     url = 'https://tieba.baidu.com/sign/add'
     bdtbqd=bdtbqds[i]
+    print("正在执行签到的贴吧：%s" % bdtbqd)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.39',
         'cookie': ("%s" % bdtbck), }
@@ -17,4 +22,26 @@ for i in range(len(bdtbqds)):
         "kw": ("%s" % bdtbqd)
     }
     html = requests.post(url=url, headers=headers, data=data).text
-    print(html.encode().decode("unicode_escape"))
+    req=json.loads(html)
+    no=(req['no'])
+    if no==0:
+        req=req['data']
+        print(req['errmsg'])
+    else:
+        print(req['error'])
+    
+    print("======================================================")
+    if i==(len(bdtbqds)-1):
+        print("")
+    else:
+        print("两秒后继续签到下一个")
+    time.sleep(2)
+
+
+start_time = time.time()
+sum = 0
+for i in range(1000000):
+    sum += i
+end_time = time.time()
+print("脚本运行结束 运行时间：%f" % (end_time - start_time))
+# print(html)
